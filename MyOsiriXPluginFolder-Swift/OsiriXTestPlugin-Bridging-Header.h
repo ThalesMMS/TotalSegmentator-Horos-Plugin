508dc3bbe661558f4d01b1770dcf60bcb63b2b59
Thales Matheus <133025183+ThalesMMS@users.noreply.github.com>
2025-10-09 22:35:27 -0300

    Update Horos plugin entry points

--- MyOsiriXPluginFolder-Swift/OsiriXTestPlugin-Bridging-Header.h
+++ MyOsiriXPluginFolder-Swift/OsiriXTestPlugin-Bridging-Header.h
@@ -3,24 +3,4 @@
 //
 
 #import <OsiriXAPI/PluginFilter.h>
-
-// You can then import different API headers to use the OsiriX functions.
-// These headers are stored in the OsiriXAPI framework folder
-
 #import <OsiriXAPI/BrowserController.h>
-#import <OsiriXAPI/DicomStudy.h>
-#import <OsiriXAPI/DicomSeries.h>
-#import <OsiriXAPI/DicomStudy+Report.h>
-#import <OsiriXAPI/DicomDatabase.h>
-#import <OsiriXAPI/WebPortal.h>
-#import <OsiriXAPI/NSManagedObject+N2.h>
-#import <OsiriXAPI/WebPortalUser.h>
-#import <OsiriXAPI/WebPortalStudy.h>
-#import <OsiriXAPI/WebPortalConnection.h>
-#import <OsiriXAPI/WebPortalConnection+Data.h>
-#import <OsiriXAPI/WebPortal.h>
-#import <OsiriXAPI/WebPortal+Email+Log.h>
-#import <OsiriXAPI/WebPortalResponse.h>
-#import <OsiriXAPI/WebPortalDatabase.h>
-#import <OsiriXAPI/AsyncSocket.h>
-#import <OsiriXAPI/QueryController.h>
